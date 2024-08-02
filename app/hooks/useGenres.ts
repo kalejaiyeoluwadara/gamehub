d0ead55c01499137617f56bd29037e7e1565428d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-02 12:36:50 +0100

    shipped genres with the app

--- app/hooks/useGenres.ts
+++ app/hooks/useGenres.ts
@@ -1,12 +1,13 @@
 // hooks/useGenres.ts
 "use client";
-import useData from "./useData";
+
+import genres from "../data/genres";
+
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, loading: false, error: null });
 
 export default useGenres;
